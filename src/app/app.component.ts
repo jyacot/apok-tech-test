0d57b8e03aa0e372a6748889e8b57e1a2318f913
Julio Yacot <jyacot@gmail.com>
2022-02-23 12:29:17 -0400

    V1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  title = 'apok-tech-test';
-
-  ngAfterViewInit() {}
-}
+export class AppComponent {}
